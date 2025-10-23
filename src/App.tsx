0ec7a79b1127f4746e06d6ebce52cf3cb9305ff3
satyakusuma <satyategar25@gmail.com>
2025-10-23 20:17:02 +0800

    add counterComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { CounterComponent } from "./componenets/CounterComponent"
+
 function App() {
 
   return (
       <div>
         <h1>Demo Redux Counter</h1>
+        <CounterComponent />
       </div>
 
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+import React from "react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { Provider } from 'react-redux';
+import { store } from './redux/store.ts';
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
+  </React.StrictMode>,
 )
